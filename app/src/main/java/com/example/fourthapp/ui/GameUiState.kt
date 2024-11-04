580cc4084e1a571c86d5cd6a806d57dd2b643806
ji-circle <sp202401jw@gmail.com>
2024-11-05 05:20:12 +0900

    first

--- app/src/main/java/com/example/fourthapp/ui/GameUiState.kt
+++ app/src/main/java/com/example/fourthapp/ui/GameUiState.kt
@@ -3,6 +3,5 @@ package com.example.fourthapp.ui
 data class GameUiState(
     val currentScrambledWord: String = "",
     val isGuessedWordWrong: Boolean = false,
-    //아래 추가
     val score: Int = 0
 )
